4fc2e1ac639359acda09f51ca088bf274d0c89e0
Shailesh Kumar <shaileshk@gmail.com>
2022-10-26 18:36:25 +0530

    error bars for individual components of the measurement vectors

--- scripts/sbsm/sbsm_sensing_stats.py
+++ scripts/sbsm/sbsm_sensing_stats.py
@@ -2,14 +2,18 @@ from skecg.apps.apputils import *
 from skecg.physionet import *
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_theme()
 from statsmodels.stats import descriptivestats
 
 import cr.sparse.dict as crdict
+import cr.sparse.lop as crlop
 
 def build_sensor(key, m, n, d):
-    Phi = crdict.rademacher_dict(key, m, n, d,
+    Phi = crdict.sparse_binary_mtx(key, m, n, d,
         normalize_atoms=False)
-    Phi = crs.lop.jit(Phi)
+    Phi = crlop.sparse_real_matrix(Phi)
     return Phi
 
 
@@ -19,11 +23,12 @@ def build_sensor(key, m, n, d):
 @click.argument('n', type=int)
 @click.argument('d', type=int)
 def main(m, n, d):
-    destination = f'cs-measurements-{m}x{n}-stats.csv'
+    destination = f'sbsm-{d}-measurements-{m}x{n}-stats.csv'
     mit_bih_dir = get_db_dir()
     record_nums = MIT_BIH['record_nums']
     all_stats = []
-    sensor = build_sensor(key, m, n)
+    sensor = build_sensor(key, m, n, d)
+    Y_vecs = []
     for record_num in record_nums:
         click.echo(f'Processing: {record_num}')
         path = f'{mit_bih_dir}/{record_num}'
@@ -38,6 +43,7 @@ def main(m, n, d):
         Y = sensor.times(X)
         # flatten measurement values
         Y_flat = np.array(Y.flatten(order='F'))
+        Y_vecs.append(Y)
         # compute stats
         stats = descriptivestats.describe(Y_flat)
         stats = stats.T
@@ -45,6 +51,7 @@ def main(m, n, d):
         stats.insert(0, 'record', [record_num])
         click.echo(stats)
         all_stats.append(stats)
+    Y_vecs = np.concatenate(Y_vecs, axis=1)
     # save results after all records
     df = pd.concat(all_stats)
     df = df.drop(columns=['missing'])
@@ -55,6 +62,15 @@ def main(m, n, d):
     df['range'] = df['range'].astype(int)
     df.to_csv(destination)
 
+    # take indices to rows
+    Y_vecs = Y_vecs.T
+    df = pd.DataFrame(Y_vecs)
+
+    means = df.mean()
+    stds = df.std()
+    fig, ax = plt.subplots(figsize=(15, 5))
+    ax.errorbar(np.arange(m), means, stds, ecolor='r')
+    fig.savefig(f'sbsm-{d}-{m}x{n}-mvec-errorbar.png') 
 
 if __name__ == '__main__':
     main()
